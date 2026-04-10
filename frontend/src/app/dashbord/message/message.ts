37ac245acb429bf2c3f367ddfcf2121a46bb89c4
hazem <bchirihazem20@gamil.com>
2026-04-10 17:44:35 +0100

    add

--- frontend/src/app/dashbord/message/message.ts
+++ frontend/src/app/dashbord/message/message.ts
@@ -1,59 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-interface Message {
-  sender: string;
-  content: string;
-  time: string;
-}
 
 @Component({
-  selector: 'app-messages',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './messages.html',
+  selector: 'app-message',
+  imports: [],
+  templateUrl: './message.html',
+  styleUrl: './message.css',
 })
-export class MessagesComponent {
-
-  selectedCoach = 'Coach Ahmed';
-
-  coaches = [
-    'Coach Ahmed',
-    'Coach Ali',
-    'Coach Sami'
-  ];
-
-  newMessage = '';
-
-  messages: Message[] = [
-    { sender: 'Coach Ahmed', content: 'Bonjour, prêt pour entraînement ?', time: '10:00' },
-    { sender: 'Moi', content: 'Oui coach 👍', time: '10:01' }
-  ];
-
-  sendMessage() {
-    if (!this.newMessage.trim()) return;
-
-    this.messages.push({
-      sender: 'Moi',
-      content: this.newMessage,
-      time: new Date().toLocaleTimeString()
-    });
-
-    this.newMessage = '';
-
-    // réponse automatique (dummy)
-    setTimeout(() => {
-      this.messages.push({
-        sender: this.selectedCoach,
-        content: 'Bien reçu 👍',
-        time: new Date().toLocaleTimeString()
-      });
-    }, 1000);
-  }
-
-  selectCoach(coach: string) {
-    this.selectedCoach = coach;
-    this.messages = []; // reset chat (dummy)
-  }
-}
+export class Message {}

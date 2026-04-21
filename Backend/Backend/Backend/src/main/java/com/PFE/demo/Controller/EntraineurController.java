d8e30fe1ec3194516c38965393edf953c9afb5fb
kais-120 <kaisbennour7@gmail.com>
2026-04-21 15:45:00 +0100

    fix and add

--- Backend/Backend/Backend/src/main/java/com/PFE/demo/Controller/EntraineurController.java
+++ Backend/Backend/Backend/src/main/java/com/PFE/demo/Controller/EntraineurController.java
@@ -1,25 +1,26 @@
 package com.PFE.demo.Controller;
 
 import com.PFE.demo.Entity.User;
+import com.PFE.demo.Service.EntraineurService;
 import com.PFE.demo.Service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user/entraineur")
 @CrossOrigin("*")
 public class EntraineurController {
 
-    private final UserService userService;
+    private final EntraineurService entraineurService;
 
-    public EntraineurController(UserService userService) {
-        this.userService = userService;
+    public EntraineurController(EntraineurService entraineurService) {
+        this.entraineurService = entraineurService;
     }
 
     // 🔥 Afficher seulement les coachs
     @GetMapping("/coachs")
     public List<User> getCoachs() {
-        return userService.getCoachs();
+        return entraineurService.getCoachs();
     }
 }
